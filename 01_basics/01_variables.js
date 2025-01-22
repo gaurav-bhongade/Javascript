89055a34fb52660106218a2e0414da2794164728
gaurav <gauravbhongade95@gmail.com>
2025-01-22 16:03:42 +0530

    files added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 123456
+let accountEmail = "gauravbhongade95@gmail.com"
+var accountPassword = "12345"
+accountCity = "Nagpur"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "gbhongade8@gmail.com"
+accountPassword = "111111"
+accountCity = "Pune"
+console.log(accountId)
+
+/*
+prefer not to use var
+because of issues in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
